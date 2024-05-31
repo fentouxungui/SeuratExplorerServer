eff5aeb3354116462ae1b9f9d480f619116bf26e
FenTouXunGui <763278134@qq.com>
2024-05-31 09:39:32 +0800

    remove examples

--- examples/shinyapps.io/app.R
+++
@@ -1,7 +0,0 @@
-if(!require(devtools)){install.packages("devtools")}
-if(!require(SeuratExplorer)){install_github("fentouxungui/SeuratExplorer")}
-if(!require(SeuratExplorerServer)){install_github("fentouxungui/SeuratExplorerServer")}
-
-launchSeuratExplorerServer(Encrypted = TRUE)
-
-
--- examples/shinyserver/app.R
+++
@@ -1,4 +0,0 @@
-if(!require(devtools)){install.packages("devtools")}
-if(!require(SeuratExplorer)){install_github("fentouxungui/SeuratExplorer")}
-if(!require(SeuratExplorerServer)){install_github("fentouxungui/SeuratExplorerServer")}
-
